32dac8358baaa280f1ecd1521962a37a56aa8220
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-15 23:01:16 -0300

    feat: add redis file methods keys set and get

--- redis.go
+++ redis.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/go-redis/redis"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/go-redis/redis"
+)
 
 var (
 	client = &redisClient{}
@@ -22,4 +27,28 @@ func NewRedisClient() *redisClient {
 	client.c = c
 
 	return client
+}
+
+func (client *redisClient) GetKey(key string, src interface{}) error {
+	val, err := client.c.Get(key).Result()
+	if err == redis.Nil || err != nil {
+		return err
+	}
+	err = json.Unmarshal([]byte(val), &src)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *redisClient) SetKey(key string, value interface{}, expiration time.Duration) error {
+	cacheEntry, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	err = client.c.Set(key, cacheEntry, expiration).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
